51b5031b36b189c9c369ae2479ced2c9501df8a9
Samrach Sophal <2024037sophal@aupp.edu.kh>
2024-03-27 17:00:15 +0700

    homework7

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,15 @@
 def remove_all_after(numbers, n):
-    ...
+    if n not in numbers:
+        return "invalid input"
+    else:
+        removed_list = []
+        for i in numbers:
+            removed_list.append(i)
+            if n == i:
+                return removed_list
+
+numbers = range(0, 101)
+print("The list is from 0 - 100")
+n = int(input("Enter removing number: "))
+
+print(remove_all_after(numbers, n))
